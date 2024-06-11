17a05e255d0c46043ac88ee9c09330868fbc8268
Bradley Abel <brad.j.abel@gmail.com>
2024-06-10 21:31:27 -0600

    first commit

---
+++ lib/hello_elixir/application.ex
@@ -0,0 +1,44 @@
+defmodule HelloElixir.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      HelloElixirWeb.Telemetry,
+      HelloElixir.Repo,
+      {Ecto.Migrator,
+        repos: Application.fetch_env!(:hello_elixir, :ecto_repos),
+        skip: skip_migrations?()},
+      {DNSCluster, query: Application.get_env(:hello_elixir, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: HelloElixir.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: HelloElixir.Finch},
+      # Start a worker by calling: HelloElixir.Worker.start_link(arg)
+      # {HelloElixir.Worker, arg},
+      # Start to serve requests, typically the last entry
+      HelloElixirWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: HelloElixir.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    HelloElixirWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+
+  defp skip_migrations?() do
+    # By default, sqlite migrations are run when using a release
+    System.get_env("RELEASE_NAME") != nil
+  end
+end
